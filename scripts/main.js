45834fd1aefbde44001fd730df3f5a330b7ebca7
Nikolay Yatsenko <yatsenkokolyan@gmail.com>
2023-02-22 14:38:21 +0600

    Added pharm page + adaptive + some fixes

--- scripts/main.js
+++ scripts/main.js
@@ -146,6 +146,18 @@ if (document.querySelector('#phone')) {
 
 
 let calendar = new AirDatepicker('#calendar', {
-    inline: false,
-    position: 'right center',
+    position: 'bottom right',
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
